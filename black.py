d17a21540c5783204da6e7a0213aa422fc2dd9b3
Tim Swast <swast@google.com>
2018-09-25 08:25:58 -0700

    Add .nox directories to default exclude (#525)
    [Nox](https://nox.readthedocs.io/) is similar to Tox. It creates a .nox directory that contains virtualenv for testing with different Python versions.
    
    (cherry picked from commit 2d99573b343cbaa721746af8df0ffb27e93140d4)

--- black.py
+++ black.py
@@ -50,7 +50,7 @@ from blib2to3.pgen2.parse import ParseError
 __version__ = "19.0.1"
 DEFAULT_LINE_LENGTH = 88
 DEFAULT_EXCLUDES = (
-    r"/(\.git|\.hg|\.mypy_cache|\.tox|\.venv|_build|buck-out|build|dist)/"
+    r"/(\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|_build|buck-out|build|dist)/"
 )
 DEFAULT_INCLUDES = r"\.pyi?$"
 CACHE_DIR = Path(user_cache_dir("tan", version=__version__))
