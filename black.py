1ea953adb33cfedde345e17b523a109f8a44cded
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2018-11-13 13:23:56 -0800

    remove unused variable (#604)
    (cherry picked from commit fbf59b60d750ace9792a78736f19a4e81a156333)

--- black.py
+++ black.py
@@ -2458,7 +2458,7 @@ def standalone_comment_split(line: Line, py36: bool = False) -> Iterator[Line]:
         nonlocal current_line
         try:
             current_line.append_safe(leaf, preformatted=True)
-        except ValueError as ve:
+        except ValueError:
             yield current_line
 
             current_line = Line(
