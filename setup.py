psf/black

patch main to ensure click_patch() gets called (#572)

setup.py
@@ -57,5 +57,10 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Quality Assurance",
     ],
-    entry_points={"console_scripts": ["black=black:main", "blackd=blackd:main [d]"]},
+    entry_points={
+        "console_scripts": [
+            "black=black:patched_main",
+            "blackd=blackd:patched_main [d]",
+        ]
+    },
 )

takeda/tan

(cherry picked from commit 5c2dd96a69a935cf45acbdf2ffabbd39b27d38fa)

setup.py
@@ -57,5 +57,10 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Quality Assurance",
     ],
-    entry_points={"console_scripts": ["tan=black:main", "tand=blackd:main [d]"]},
+    entry_points={
+        "console_scripts": [
+            "tan=black:patched_main",
+            "tand=blackd:patched_main [d]",
+        ]
+    },
 )